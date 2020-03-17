fc56794d9da34b926770299c13d1afe709f374cf
chermehdi <mehdi.cheracher@gmail.com>
2020-03-17 16:18:17 +0000

    Add configuration for building a spring boot application

---
+++ contest-manager/src/main/java/io/zulus/cm/ZulusContestManagerApplication.java
@@ -0,0 +1,13 @@
+package io.zulus.cm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZulusContestManagerApplication { 
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZulusContestManagerApplication.class, args);
+	}
+
+}
---
+++ contest-manager/src/test/java/io/zulus/cm/ContestManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.zulus.cm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContestManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
